23d51701467d4964cf12cb60343035859921c963
dkyrgia <dkyrgia@gmail.com>
2023-02-13 11:43:56 +0200

    just rename files without getting filesystem directory list

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import openpyxl
 import os
 
@@ -25,34 +21,16 @@ max_rows = sheet_obj.max_row
 print(max_rows)
 print(max_cols)
 
-fileFromPdf = [0] * max_rows
-
 for i in range(1, max_rows):
     # print(i)
-    name_obj = sheet_obj.cell(row=i, column=2)
-    mail_obj = sheet_obj.cell(row=i, column=3)
-    fileFromPdf[i-1] = int(pdfList[i-1].split('.')[0])
-    # fileFromPdf[i-1] = pdfList[i-1]
-    # aaa = pdfList[i-1].split('-')[0]
-    # print(aaa, i-1)
-    # if int(aaa) == i:
-    #     print(pdfList[i-1].split('-')[0], " ela", i)
-    # print("#1")
-    # print("fileFromPdf", fileFromPdf[i])
-    # print("#2")
-    # print("pdfList", pdfList[i].split('-', 1), i)
-    # print(name_obj.value)
-print(fileFromPdf)
-fileFromPdf.sort()
+    name_obj = sheet_obj.cell(row=i + 1, column=2).value
+    mail_obj = sheet_obj.cell(row=i + 1, column=3).value
+    oldname = "pdfs\\" + str(i) + ".pdf"
+    newname = "pdfs\\" + name_obj + ".pdf"
+
+    os.rename(oldname, newname)
+
 pdfList.insert(0, 0)
-for x in range(0, max_rows):
-
-    print("fileFromPDF", fileFromPdf[x], " ", x, "name = ", pdfList[x])
-# print(pdfList)
-
-
-
-    # print(cell_obj.value)
 
 
 def print_hi(name):
@@ -63,5 +41,3 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
